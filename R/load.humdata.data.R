6509413fdb35c47acf97dc7eaf4f8ac3570d19d5
Stefano Ghirlanda <drghirlanda@gmail.com>
2020-03-29 05:10:13 -0400

    Downloading humdata in wide format to save time

--- R/load.humdata.data.R
+++ R/load.humdata.data.R
@@ -1,29 +1,33 @@
 load.humdata.data <- function() {
 
-    confirmed <- fread("https://data.humdata.org/hxlproxy/data/download/time_series_covid19_confirmed_global_narrow.csv?dest=data_edit&filter01=explode&explode-header-att01=date&explode-value-att01=value&filter02=rename&rename-oldtag02=%23affected%2Bdate&rename-newtag02=%23date&rename-header02=Date&filter03=rename&rename-oldtag03=%23affected%2Bvalue&rename-newtag03=%23affected%2Binfected%2Bvalue%2Bnum&rename-header03=Value&filter04=clean&clean-date-tags04=%23date&filter05=sort&sort-tags05=%23date&sort-reverse05=on&filter06=sort&sort-tags06=%23country%2Bname%2C%23adm1%2Bname&tagger-match-all=on&tagger-default-tag=%23affected%2Blabel&tagger-01-header=province%2Fstate&tagger-01-tag=%23adm1%2Bname&tagger-02-header=country%2Fregion&tagger-02-tag=%23country%2Bname&tagger-03-header=lat&tagger-03-tag=%23geo%2Blat&tagger-04-header=long&tagger-04-tag=%23geo%2Blon&header-row=1&url=https%3A%2F%2Fraw.githubusercontent.com%2FCSSEGISandData%2FCOVID-19%2Fmaster%2Fcsse_covid_19_data%2Fcsse_covid_19_time_series%2Ftime_series_covid19_confirmed_global.csv")
+    confirmed <- fread("https://data.humdata.org/hxlproxy/api/data-preview.csv?url=https%3A%2F%2Fraw.githubusercontent.com%2FCSSEGISandData%2FCOVID-19%2Fmaster%2Fcsse_covid_19_data%2Fcsse_covid_19_time_series%2Ftime_series_covid19_confirmed_global.csv&filename=time_series_covid19_confirmed_global.csv")
+    
     confirmed$What <- "Confirmed Cases"
 
-    deaths <- fread("https://data.humdata.org/hxlproxy/data/download/time_series_covid19_deaths_global_narrow.csv?dest=data_edit&filter01=explode&explode-header-att01=date&explode-value-att01=value&filter02=rename&rename-oldtag02=%23affected%2Bdate&rename-newtag02=%23date&rename-header02=Date&filter03=rename&rename-oldtag03=%23affected%2Bvalue&rename-newtag03=%23affected%2Binfected%2Bvalue%2Bnum&rename-header03=Value&filter04=clean&clean-date-tags04=%23date&filter05=sort&sort-tags05=%23date&sort-reverse05=on&filter06=sort&sort-tags06=%23country%2Bname%2C%23adm1%2Bname&tagger-match-all=on&tagger-default-tag=%23affected%2Blabel&tagger-01-header=province%2Fstate&tagger-01-tag=%23adm1%2Bname&tagger-02-header=country%2Fregion&tagger-02-tag=%23country%2Bname&tagger-03-header=lat&tagger-03-tag=%23geo%2Blat&tagger-04-header=long&tagger-04-tag=%23geo%2Blon&header-row=1&url=https%3A%2F%2Fraw.githubusercontent.com%2FCSSEGISandData%2FCOVID-19%2Fmaster%2Fcsse_covid_19_data%2Fcsse_covid_19_time_series%2Ftime_series_covid19_deaths_global.csv" )
+    deaths <- fread("https://data.humdata.org/hxlproxy/api/data-preview.csv?url=https%3A%2F%2Fraw.githubusercontent.com%2FCSSEGISandData%2FCOVID-19%2Fmaster%2Fcsse_covid_19_data%2Fcsse_covid_19_time_series%2Ftime_series_covid19_deaths_global.csv&filename=time_series_covid19_deaths_global.csv")
+    
     deaths$What <- "Fatalities"
 
     covid <- rbind( confirmed, deaths )
     setnames(
         covid,
-        c("Province/State","Country/Region","Date","Value"),
-        c("Reg2",     "Reg1",        "Day", "Count")
+        c("Province/State", "Country/Region"),
+        c("Reg2", "Reg1")
     )
+    ## discard Long and Lat
+    covid[ , Long := NULL ]
+    covid[ , Lat  := NULL ]
+
+    covid <- melt( covid, id.vars=c("Reg1","Reg2","What") )
+    setnames( covid, c("variable","value"), c("Day","Count") )
+
     covid$Day <- as.Date( covid$Day, format="%F" )
     covid$Count <- as.numeric( covid$Count )
-    ## discard some header information
-    covid <- covid[ ! grep("#",Reg1) ]
+
     ## replace empty subregion with All
     covid[ Reg2=="", Reg2 := "All" ]
     ## discard Count==0
     covid <- covid[ Count>0 ]
-
-    ## discard Long and Lat
-    covid[ , Long := NULL ]
-    covid[ , Lat  := NULL ]
 
     ## add Reg2 := All to Reg1 entities that lack it
     no.all <- covid[ , sum(Reg2=="All"), by=Reg1 ][ V1==0, Reg1 ]
--- getData.R
+++ getData.R
@@ -1,12 +1,16 @@
 ## assemble our covid data set from various sources
 
-library(data.table)
-dummy <- lapply( dir("R",pattern="R$",full.names=TRUE), source )
+suppressPackageStartupMessages({
+    library(data.table)
+    dummy <- lapply( dir("R",pattern="R$",full.names=TRUE), source )
+})
 
 ## load global data from data.humdata.org:
+message( "loading world data" )
 covid <- load.humdata.data()
 
 ## load New York Times data for US states and counties
+message( "loading US data" )
 us <- load.us.states()
 covid <- rbind( covid, us )
 us.c <- load.us.counties()
@@ -15,6 +19,7 @@ covid <- rbind( covid, us.c )
 
 ## merge official Italian data.  we keep the world data when no
 ## official Italian data are available.
+message( "loading Italian data" )
 italy <- load.italian.data()
 days <- unique( italy$Day )
 covid <- covid[
@@ -24,17 +29,20 @@ covid <- covid[
 covid <- rbind( covid, italy )
 
 ## merge official French data
+message( "loading French data" )
 fr <- load.french.data()
 covid <- covid[ ! Reg1=="France" ]
 covid <- rbind( covid, fr )
 
 ## merge Spanish data
+message( "loading Spanish data" )
 sp <- load.spanish.data()
 days <- sp[ Reg2=="All", Day ]
 covid <- covid[ ! (Reg1=="Spain" & Day %in% days) ]
 covid <- rbind( covid, sp )
 
 ## save a sorted data.table so that we don't have to sort in app
+message( "sorting data" )
 covid[
     What=="Fatalities",
     maxCount := max(Count),
@@ -48,4 +56,5 @@ covid[, maxCount := NULL ]
 covid <- covid[ Count>0 ]
 
 ## done!
+message( "saving" )
 fwrite( covid, "covid.csv" )
